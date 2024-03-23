30aed2d5c628e445c460bfaa4882de2121f23503
XZohaib01 <162486992+XZohaib01@users.noreply.github.com>
2024-03-23 14:50:39 -0700

    This is a Weight Calculator.

---
+++ app.js
@@ -0,0 +1,4 @@
+var weightInKg = 70; // 70kg
+var hightInMeters = 5.6; // 5.6m
+var bmi = weightInKg / (hightInMeters * hightInMeters);
+console.log("your BMI is ".concat(bmi));
---
+++ app.ts
@@ -0,0 +1,4 @@
+let weightInKg = 70; // 70kg
+let hightInMeters = 5.6; // 5.6m
+let bmi = weightInKg / (hightInMeters * hightInMeters);
+console.log (`your BMI is ${bmi}`);
